4dc5391f678d9e85f178c723a3170af37e683561
Elena Khramtsova <helen.andrianova@gmail.com>
2022-06-29 01:52:11 +0200

    remove .temp folder

--- .temp/modules/TIP4_3/interfaces/~IIndexBasis.sol
+++
@@ -1,6 +0,0 @@
-pragma ton-solidity >= 0.57.1;
-
-interface IIndexBasis {
-    function getInfo() external view responsible returns (address collection);
-    function destruct(address gasReceiver) external;
-}
